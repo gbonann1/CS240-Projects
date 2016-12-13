33a1fa686bd8a5c20fc8d1984abf40ba1e5f92d1
gbonann1 <lucaisfly@optonline.net>
2016-12-13 02:00:14 -0500

    Update Main.cpp

--- Program2Gbonann1/Main.cpp
+++ Program2Gbonann1/Main.cpp
@@ -34,22 +34,7 @@ int main(){
 		players.push_front(p1);
 		players.push_front(p2);
 	}
-	/**if (players.size() == 1 || players.size() == 2 || players.size() == 4 || players.size() == 8 || players.size() == 16 || 
-		players.size() == 32 ||	players.size() == 64 || players.size() == 128 || players.size() == 256 || players.size() == 512 || players.size() == 1024 ||
-	   	players.size() == 2048 || players.size() == 4096 || players.size() == 8192 || players.size() == 16384 ||
-	  	players.size() == 32768 || players.size() == 65536 || players.size() == 131072 || players.size() == 262144 ||
-	  	players.size() == 524288 || players.size() == 1048576 || players.size() == 2097152 ||
-	  	players.size() == 4194304 || players.size() == 8388608 || players.size() == 16777216 ||
-	  	players.size() == 33554432 || players.size() == 67108864 || players.size() == 134217728 ||
-	  	players.size() == 268435456 || players.size() == 536870912 || players.size() == 1073741824 ||
-	  	players.size() == 2147483648){
-		Game warTournament = Game(players);
-		warTournament.play();
-	}
-	else {
-		cout << "Tournament must have 2^n players";
-	}
-	**/
+	
 	Game warTournament = Game(players);
 	warTournament.play();
 	
